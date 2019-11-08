cbd345d79188693c9d8e64f983d1aa259830ba4f
Benjamin Herrmann <beherrma@htwg-konstanz.de>
2019-11-08 15:51:37 +0100

    added first impl Player with Test

--- src/main/scala/stratego/model/Player.scala
+++
@@ -1,5 +0,0 @@
-package stratego.model
-
-class Player {
-  //TODO
-}
--- src/test/scala/stratego/model/PlayerTest.scala
+++ src/test/scala/stratego/model/PlayerTest.scala
@@ -1,7 +1,18 @@
 package stratego.model
 
-import org.scalatest.WordSpec
+import org.scalatest.{Matchers, WordSpec}
+import stratego.model.playerComponent.Player
 
-class PlayerTest extends WordSpec {
-  // TODO
+class PlayerTest extends WordSpec with Matchers {
+  "A Player" when {
+    "new" should {
+      val player = Player("Name")
+      "has a name" in {
+        player.name should be("Name")
+      }
+      "has a String representation" in {
+        player.toString should be("Name")
+      }
+    }
+  }
 }
