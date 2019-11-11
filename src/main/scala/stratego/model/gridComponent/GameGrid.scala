e2694f247efed37d279020ae5b408fd04dd9d41f
LasterSlaster <lasterslaster@gmx.de>
2019-11-12 00:39:06 +0100

    Temp fix errors

--- src/main/scala/stratego/model/gridComponent/GameGrid.scala
+++ src/main/scala/stratego/model/gridComponent/GameGrid.scala
@@ -1,11 +1,14 @@
 package stratego.model.gridComponent
 
+import stratego.model
+import stratego.model.gridComponent
+import stratego.model.playerComponent.Player
+
 import scala.stratego.model.gridComponent.{FieldType, Matrix}
 import scala.math.sqrt
-import scala.reflect.api.Position
 
 case class GameGrid(grid: Matrix[Field]) extends GridInterface{
-  def this(size: Int) = this(new Matrix[Field](size, Field(FieldType.EMPTY_FIELD)))
+  def this(size: Int) = this(new Matrix[Field](size, Field(FieldType.EMPTY_FIELD, new Figure.NoFigure(new Player("game")))))
 
   val size: Int = grid.size
   val sizeRowCol: Int = sqrt(size).toInt
@@ -16,20 +19,20 @@ case class GameGrid(grid: Matrix[Field]) extends GridInterface{
     for {
       row <- 0 until 4
       col <- 0 until 10
-    } gameGrid = copy(grid.replaceField(row, col, Field(FieldType.A_SIDE)))
+    } gameGrid = copy(grid.replaceField(row, col, Field(FieldType.A_SIDE, new Figure.NoFigure(new Player("game")))))
     for {
       row <- 6 until 10
       col <- 0 until 10
-    } gameGrid = copy(grid.replaceField(row, col, Field(FieldType.B_SIDE)))
+    } gameGrid = copy(grid.replaceField(row, col, Field(FieldType.B_SIDE, new Figure.NoFigure(new Player("game")))))
 
-    gameGrid = copy(grid.replaceField(4, 2, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(4, 3, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(5, 2, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(5, 3, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(4, 6, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(4, 7, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(5, 6, Field(FieldType.NO_FIELD)))
-    gameGrid = copy(grid.replaceField(5, 7, Field(FieldType.NO_FIELD)))
+    gameGrid = copy(grid.replaceField(4, 2, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(4, 3, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(5, 2, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(5, 3, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(4, 6, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(4, 7, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(5, 6, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
+    gameGrid = copy(grid.replaceField(5, 7, Field(FieldType.NO_FIELD, new Figure.NoFigure(new Player("game")))))
     gameGrid
   }
 
