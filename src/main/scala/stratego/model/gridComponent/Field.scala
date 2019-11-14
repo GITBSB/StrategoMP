65aa68f917085e6291b8fcb6ad117b97229c45dd
LasterSlaster <lasterslaster@gmx.de>
2019-11-14 13:42:36 +0100

    remove duplicate constructor

--- src/main/scala/stratego/model/gridComponent/Field.scala
+++ src/main/scala/stratego/model/gridComponent/Field.scala
@@ -2,7 +2,5 @@ package stratego.model.gridComponent
 import scala.stratego.model.gridComponent.FieldType._
 
 case class Field(fieldType: FieldType, figure: Figure) extends FieldInterface() {
-  def this(fieldType: FieldType, figure: Figure) = this(fieldType, figure)
-
   override def setFigure(figure: Figure): Field = copy(fieldType, figure)
 }
