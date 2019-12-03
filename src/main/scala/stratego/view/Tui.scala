1e6f0bd1b92864f8c77fb4cf6be9d426c5f1ed3c
LasterSlaster <lasterslaster@gmx.de>
2019-12-03 13:57:07 +0100

    fix Tui reactions

--- src/main/scala/stratego/view/Tui.scala
+++ src/main/scala/stratego/view/Tui.scala
@@ -1,21 +1,20 @@
 package stratego.view
 
 import com.typesafe.scalalogging.LazyLogging
-import stratego.model.engineComponent.GameEngineInterface
+import stratego.model.engineComponent.{GameEngineInterface, GameEvents}
 import stratego.model.engineComponent.GameEvents.{GameQuitEvent, GameStartedEvent}
 
 import scala.swing.Reactor
 
 class Tui(gameEngine: GameEngineInterface) extends LazyLogging with Reactor {
 
-  //TODO: Check how to access Event
   listenTo(gameEngine)
   reactions += {
-    case event: GameStartedEvent=> printTui
+    case event: GameStartedEvent.type => printTui
    //case event: ShutdownStratego => stopProcessingInput = true
   }
 
   def printTui: Unit = {
-    logger.info("printTui")
+    logger.info(gameEngine.gridToString)
   }
 }
