b69781293a2bbade50f2ecd6b07f8cd2adbbf9f1
LasterSlaster <lasterslaster@gmx.de>
2019-12-16 20:55:05 +0100

    Add PlayerType

--- src/main/scala/stratego/model/engineComponent/ActivePlayer.scala
+++
@@ -1,7 +0,0 @@
-package stratego.model.engineComponent
-
-object ActivePlayer extends Enumeration {
-  type ActivePlayer = Value
-  val PLAYER_A = Value("PlayerA")
-  val PLAYER_B = Value("PlayerB")
-}
